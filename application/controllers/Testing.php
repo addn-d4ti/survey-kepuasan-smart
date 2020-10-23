4d16cd4efff19e836ca8a9252dedc3a3eaf69361
dzikrialifa <dzikrialif99@gmail.com>
2020-10-23 09:33:40 +0700

    Testing controllers

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -15,10 +15,15 @@ class Testing extends CI_Controller {
 
     public function listkaryawan()
     {
-        $this->load->view('templates/header'); // load header
-        $this->load->view('templates/sidebar'); // load sidebar
-        $this->load->view('templates/topbar'); // load topbar
-        $this->load->view('admin/listkaryawan'); // LOAD CONTENT
-        $this->load->view('templates/footer'); // load footer
+        $this->load->view('templates/header'); 
+        $this->load->view('templates/sidebar'); 
+        $this->load->view('templates/topbar'); 
+        $this->load->view('admin/listkaryawan'); 
+        $this->load->view('templates/footer'); 
+    }
+
+    public function editkaryawan()
+    {
+
     }
 }
